5f2a480a6a33620c977a15f5ce026be85c7e19bc
Stefan Schärmeli <info@schaermu.ch>
2024-04-09 14:09:54 +0200

    test: adds generic api server factory

--- api_test.go
+++ api_test.go
@@ -1,3 +1,5 @@
+// SPDX-FileCopyrightText: 2024 Stefan Schärmeli <schaermu@pm.me>
+// SPDX-License-Identifier: MIT
 package main
 
 import (
@@ -8,29 +10,35 @@ import (
 	"testing"
 )
 
-func CreateTestApiServer(t *testing.T, url string, payloadFile string) *httptest.Server {
-	var expected []byte
-	var err error
-	if payloadFile == "" {
-		expected = []byte("OK")
-	} else {
-		expected, err = os.ReadFile(payloadFile)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
+func CreateSimpleTestApiServer(t *testing.T, url string, payloadFile string) *httptest.Server {
+	return CreateTestApiServer(t, map[string]string{url: payloadFile})
+}
 
+func CreateTestApiServer(t *testing.T, urlPayloadMap map[string]string) *httptest.Server {
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		// Test request parameters
-		equals(t, req.URL.String(), url)
-		// Send response to be tested
-		rw.Write(expected)
+		payloadFile, ok := urlPayloadMap[req.URL.String()]
+		if !ok {
+			// return 404 for unknown requests
+			rw.WriteHeader(http.StatusNotFound)
+		} else {
+			if payloadFile == "" {
+				rw.Write([]byte("OK"))
+			} else {
+				expected, err := os.ReadFile(payloadFile)
+				if err != nil {
+					rw.WriteHeader(http.StatusNotFound)
+				} else {
+					rw.Write(expected)
+				}
+			}
+
+		}
 	}))
 	return server
 }
 
 func TestGetRequestApiKey(t *testing.T) {
-	server := CreateTestApiServer(t, "/api/v1/watch", "")
+	server := CreateSimpleTestApiServer(t, "/api/v1/watch", "")
 	defer server.Close()
 
 	api := NewApiClient(server.URL, "foo-bar-key")
@@ -41,7 +49,7 @@ func TestGetRequestApiKey(t *testing.T) {
 
 func TestGetWatches(t *testing.T) {
 	// Start a local HTTP server
-	server := CreateTestApiServer(t, "/api/v1/watch", "./test/json/getWatches.json")
+	server := CreateSimpleTestApiServer(t, "/api/v1/watch", "./test/json/getWatches.json")
 	defer server.Close()
 
 	api := NewApiClient(server.URL, "foo-bar-key")
@@ -55,12 +63,12 @@ func TestGetLatestPriceSnapshot(t *testing.T) {
 	id := "6a4b7d5c-fee4-4616-9f43-4ac97046b595"
 
 	// Start a local HTTP server
-	server := CreateTestApiServer(t, fmt.Sprintf("/api/v1/watch/%s/history/latest", id), fmt.Sprintf("./test/json/getLatestPriceSnapshot_%s.json", id))
+	server := CreateSimpleTestApiServer(t, fmt.Sprintf("/api/v1/watch/%s/history/latest", id), fmt.Sprintf("./test/json/getLatestPriceSnapshot_%s.json", id))
 	defer server.Close()
 
 	api := NewApiClient(server.URL, "foo-bar-key")
 	priceData, err := api.getLatestPriceSnapshot("6a4b7d5c-fee4-4616-9f43-4ac97046b595")
 	ok(t, err)
-	equals(t, int32(100), priceData.Price)
+	equals(t, float64(100), priceData.Price)
 	equals(t, "USD", priceData.Currency)
 }
